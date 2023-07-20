3e84d10e2fc33f9e3f05a53ed1a258d9c7de8745
drog41813 <daniel.lee042003@gmail.com>
2023-07-20 17:12:38 -0500

    installed inquirer and created questions array

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 // TODO: Include packages needed for this application
+var inquirer = require('inquirer');
+inquirer
+    .prompt([
 
+    ])
+    .then((answers) => {
+
+    })
+    .catch((error) => {
+        if (error.isTtyError) {
+
+        } else {
+
+        }
+    });
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ["Description", "Table of Contents", "Installation", "Usage", "License", "Contributing", "Tests", "Questions"];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}

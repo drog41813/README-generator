6ab2eb11308862a27d05cd7a15c309a7597406ef
drog41813 <daniel.lee042003@gmail.com>
2023-07-23 23:20:13 -0500

    readjusted some of index.js code

--- index.js
+++ index.js
@@ -1,109 +1,80 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const fs = require('fs')
+const fs = require('fs');
+const generateMarkdown = require("./utils/generateMarkdown.js");
+
 // TODO: Create an array of questions for user input
 const questions = [
-    {
-        type: 'input',
-        name: 'title',
-        message: 'What do you want the name of the title?',
-    },
-    {
-        type: 'input',
-        name: 'Description',
-        message: 'What is the description of your project',
-    },
-    {
-        type: 'input',
-        name: 'Installation',
-        message: 'Enter installation details',
-    },
-    {
-        type: 'input',
-        name: 'usage',
-        message: 'enter usage informaiton',
-    },
-    {
-        type: 'input',
-        name: 'contributing',
-        message: 'enter any contributions',
-    },
-    {
-        type: 'input',
-        name: 'tests',
-        message: 'enter any test instructions for the project',
-    },
-    {
-        type: 'list',
-        name: 'licenses',
-        message: 'What type of license do you want for your project',
-        choices: ['MIT', 'GPLv3', 'Apache', 'unlicense', 'BSD'],
-    },
-    {
-        type: 'input',
-        name: 'github',
-        message: 'enter your GitHub username',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'enter your email address',
-    },
-].then((response)=>
-console.log(response)
-);
+  {
+    type: 'input',
+    name: 'title',
+    message: "What's the title of your project?"
+  }, 
+  {
+    type: 'input',
+    name: 'description',
+    message: "Enter the description of your project."
+  },
+  {
+    type: 'input',
+    name: 'installation',
+    message: "Enter intallation details."
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: "Enter usage information."
+  },
+  {
+    type: 'list',
+    name: 'license',
+    message: "Which license would you like for your project?",
+    choices: ['MIT', 'GPLv3', 'Apache', 'BSD', 'Unlicensed']
+  },
+  {
+    type: 'input',
+    name: 'contributing',
+    message: "Enter any contributions."
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: "Enter any tests required for your project."
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Enter your GitHub username.',
+},
+{
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address.',
+}]
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    const content = typeof date ==='string' ? data : JSON.stringify(data, null, 2);
-    fs.writeFile(fileName, content, 'utf8', (err) =>{
-        if(err){
-            console.error('error making file: ', err);
-        }
-        else{
-            console.log('File successfully created: ', fileName);
-        }
-    });
+  const content = typeof data ==='string' ? data : JSON.stringify(data, null);
+
+  fs.writeFile(fileName, content, (err) => {
+    if(err) {
+      console.error('error making file: ', err);
+    } else {
+      console.log('File successfully created: ', fileName);
+    }
+  })
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    inquirer
-    .prompt(questions)
-    .then((responses)=>{
-        const readMEContent = generateReadMEContent(responses);
-        writeToFile('README.md', readMEContent);
-    })
-    .catch((error)=>{
-        console.error('error:', error);
-    })
+  inquirer
+  .prompt(questions)
+  .then((data)=> {
+    writeToFile('README.md', generateMarkdown(data));
+  })
+  .catch((error)=> {
+    console.error('error: ', error);
+  })
 }
-function generateReadMEContent(responses) {
-    return `
-  # ${responses.title}
-  ## Description
-  ${responses.Description}
-  ## Table of Contents
-  - [Installation](#installation)
-  - [Usage](#usage)
-  - [Contributing](#contributing)
-  - [Tests](#tests)
-  - [License](#license)
-  - [Questions](#questions)
-  ## Installation
-  ${responses.Installation}
-  ## Usage
-  ${responses.usage}
-  ## Contributing
-  ${responses.contributing}
-  ## Tests
-  ${responses.tests}
-  ## License
-  This project is licensed under the ${responses.licenses} license.
-  ## Questions
-  For any questions about the project, please feel free to reach out to [${responses.github}](https://github.com/${responses.github}) or contact via email at ${responses.email}.
-  `;
-  }
 
-// Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -10,17 +10,62 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  let licenseLink;
+  switch(license){
+    case 'MIT': licenseLink = 'https://mit-license.org/';
+    break;
+    case 'BSD': licenseLink = 'https://opensource.org/licenses/BSD-3Clause';
+    break;
+    case 'GPL': licenseLink = 'https://www.gnu.org/licenses/gpl-3.0.en.html';
+    break;
+    case 'Apache': licenseLink ='https://www.apache.org/licenses/LICENSE-2.0.html';
+    break;
+    case 'Unlicensed': licenseLInk = '';
+    break;
+    default: licenseLink = 'https';
+    break;
+  }
+  return licenseLink;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license){
+  let LicenseSection = '';
+
+  if(license != 'none') {
+    LicenseSection += "##License\n";
+    LicenseSection += "See the" + renderLicenseLink(license) + "in order to get more information for the license\n";
+   }
+   return LicenseSection;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-`;
+  ## Description
+  ${data.description}
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [License](#license)
+  - [Questions](#questions)
+  ## Installation
+  ${data.Installation}
+  ## Usage
+  ${data.usage}
+  ## Contributing
+  ${data.contributing}
+  ## Tests
+  ${data.tests}
+  ## License
+  This project is licensed under the ${data.licenses} license.
+  ## Questions
+  For any questions about the project, please feel free to reach out to [${data.github}](https://github.com/${data.github}) or contact via email at ${data.email}.
+  `;
 }
 
 module.exports = generateMarkdown;
